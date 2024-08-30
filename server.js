87edd59ed57a5fc3c1e4d9a130ebeeadaf848c3b
hudaifa mohamedmana <hudaifamana23@gmail.com>
2024-08-30 13:23:12 -0400

    working copy

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+// -----------------
 //-----------------------------------
 app.get(`/`,(req,res)=>{
-    res.send = 'wolcom home'
+    res.send ('hello world')
 })
 app.get(`/fruits`,(req,res)=>{
-    res.send = 'fruits'
+    res.send ('fruits')
 })
 
 app.get(`/vig`,(req,res)=>{
-    res.send = 'vig'
+    res.send('vig')
 })
-// app.get(`/home`,(req,res)=>{
-//     res = render('home')
-// })
+app.get(`/home`,(req,res)=>{
+    res.render('home')
+})
 
 //---------------------------------
 app.set('view engine','ejs')
@@ -22,7 +23,7 @@ app.set('view engine','ejs')
 
 
 //==========================-------------------------------------------------------------------===================
-app.listen('3000'|| process.env.PORT,()=>{
+app.listen('3000',()=>{
     console.log('Server is now running 3000')
 })
 
